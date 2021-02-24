738c01f41587201f3b1709eb4d8673d68e348fd8
Ccctine <cmqarendain@iskolarngbayan.pup.edu.ph>
2021-02-24 08:40:36 +0800

    Fix Pause System, Update logo

---
+++ Assets/Resume.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Resume : MonoBehaviour
+{
+    public GameObject playerBullet , enemyBullet;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Time.timeScale = 1f;
+        playerBullet.SetActive(true);
+        enemyBullet.SetActive(true);
+    }
+}
