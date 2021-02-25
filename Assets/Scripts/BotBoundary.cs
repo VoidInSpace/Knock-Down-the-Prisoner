b2e0c03f55e323d58fbfb97955ade20d6a4171c4
VoidInSpace <amlagniton@iskolarngbayan.pup.edu.ph>
2021-02-25 21:28:41 +0800

    final modification

--- Assets/Scripts/BotBoundary.cs
+++ Assets/Scripts/BotBoundary.cs
@@ -8,6 +8,6 @@ public class BotBoundary : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(Mathf.Clamp(transform.position.x, 3f, 11.5f),Mathf.Clamp(transform.position.y, -3.5f, 3.5f), transform.position.z);
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, 3f, 7.8f),Mathf.Clamp(transform.position.y, -3.5f, 3.5f), transform.position.z);
     }
 }
