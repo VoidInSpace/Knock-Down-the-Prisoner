4b24321511d3f9ce8665657e7155922e6e670cf4
VoidInSpace <amlagniton@iskolarngbayan.pup.edu.ph>
2021-02-25 16:26:08 +0800

    modified the cans, tutorial and background

--- Assets/Scripts/PlayerBoundary.cs
+++ Assets/Scripts/PlayerBoundary.cs
@@ -8,6 +8,6 @@ public class PlayerBoundary : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -12f, -3f),Mathf.Clamp(transform.position.y, -3.5f, 3.5f), transform.position.z);
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -11.5f, -3f),Mathf.Clamp(transform.position.y, -3.5f, 3.5f), transform.position.z);
     }
 }
